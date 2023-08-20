54216b0478d50dc82a3106d5b8b956ab9070e353
RetricSu <inneverland2013@gmail.com>
2023-08-20 15:19:36 +0800

    chore: allow cors (#1)

--- server/server.go
+++ server/server.go
@@ -18,28 +18,43 @@ func NewServer() *Server {
 	}
 }
 
+func setCorsHeaders(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func (s *Server) Func(f func(s *Server)) *Server {
 	f(s)
 	return s
 }
 
-// HandleFunc 注册访问路由
+// HandleFunc registers access routes
 func (s *Server) HandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
 	s.mux.HandleFunc(pattern, handler)
 }
 
-// Handle 注册访问路由
+// Handle registers access routes
 func (s *Server) Handle(pattern string, handler http.Handler) {
 	s.mux.Handle(pattern, handler)
 }
 
 func (s *Server) HttpRun(addr string) {
 	s.http.Addr = addr
-	s.http.Handler = s.mux
+	s.http.Handler = setCorsHeaders(s.mux) // Wrap the mux with CORS headers
 
 	logrus.Debugf("http listen %s", addr)
 
-	// 启动监听
+	// Start listening
 	err := s.http.ListenAndServe()
 	if err != nil {
 		panic(err)
